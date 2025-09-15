4567db109f51f439852f6e7dfb82988c99c49d42
manvir singh judge <judgemandy312@gmail.com>
2025-09-15 15:11:04 -0700

    HomeBase--Manvir-250915-151104

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -47,3 +47,7 @@ alias neobean="NVIM_APPNAME=linkarzu/dotfiles-latest/neovim/neobean nvim"
 # Added by Windsurf
 export PATH="/Users/manvirjudge/.codeium/windsurf/bin:$PATH"
 autoload -Uz compinit && compinit
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
